75499c4ea40d2446e49448fd887bee6082f3b3be
Isaak Haas <isaakhaas96@gmail.com>
2022-02-15 22:33:05 -0600

    feat: creation of reducers

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,63 @@
+import { useState, useEffect } from 'react';
+import { fetchDates, fetchAirports } from '../store/reducer';
+import { useDispatch, useSelector } from 'react-redux';
 
+const selectDates = (state) => {
+  const { dates } = state;
+  return dates;
+};
+const availableAirports = (state) => {
+  const {
+    airports: { reducer },
+  } = state;
+  return reducer;
+};
 
+const airportsStatus = (state) => state.airports.fetchStatus;
 
 const Dashboard = () => {
-  return <h1>Hola mundo</h1>
-}
+  const [hide, setHide] = useState(true);
+  const dispatch = useDispatch();
+  const flyDates = useSelector(selectDates);
+  const airports = useSelector(availableAirports);
+  const airportsStatusSelector = useSelector(airportsStatus);
 
+  
+  useEffect(() => {
+    dispatch(fetchDates());
+    dispatch(fetchAirports());
+  }, []);
+
+  if (
+    airportsStatusSelector.loading === 'pending' ||
+    airportsStatusSelector.loading === 'idle'
+  ) {
+    return <h1>Loading</h1>;
+  }
+
+  return (
+    <form>
+      <div>
+        <input
+          type='text'
+          placeholder='Origin'
+          onClick={() => setHide(!hide)}
+        />
+        <input type='text' placeholder='Destination' />
+      </div>
+      <div hidden={hide}>
+        <ul>
+          {airports.map((x, i) => {
+            return (
+              <li id={x.code} key={i} onClick={(e) => console.log(e.target.id)}>
+                <p>{x.code}</p> <p>{x.name}</p>{' '}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </form>
+  );
+};
 
 export default Dashboard;
---
+++ src/store/reducer.js
@@ -0,0 +1,89 @@
+import axios from 'axios';
+import { combineReducers } from 'redux';
+
+// const initialState = {
+//   availableDates: {},
+//   airports: {},
+// };
+
+export const asyncMiddleware = (store) => (next) => (action) => {
+  if (typeof action === 'function') {
+    return action(store.dispatch, store.getState);
+  }
+  next(action);
+};
+
+export const fetchDates = () => async (dispatch) => {
+  dispatch({ type: 'dates/pending' });
+  try {
+    const {
+      data: {
+        data: { availableDates },
+      },
+    } = await axios.get('http://localhost:3001/available-dates');
+    const dates = availableDates.slice(0, 20);
+    dispatch({ type: 'dates/fulfilled', payload: dates });
+  } catch (error) {
+    dispatch({ type: 'dates/error', error: error.message });
+    console.log(error);
+  }
+};
+
+export const fetchAirports = () => async (dispatch) => {
+  dispatch({ type: 'airports/pending' });
+  try {
+    const {
+      data: {
+        data: { airports },
+      },
+    } = await axios.get('http://localhost:3001/airports');
+    dispatch({ type: 'airports/fulfilled', payload: airports });
+  } catch (error) {
+    dispatch({ type: 'airports/error', error: error.message });
+  }
+};
+
+const availableDatesReducer = (state = {}, action) => {
+  debugger;
+  switch (action.type) {
+    case 'dates/fulfilled':
+      return { ...state, loading: 'succeeded' };
+
+    default:
+      return state;
+  }
+};
+
+const airportsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'airports/fulfilled': {
+      return action.payload;
+    }
+    default:
+      return state;
+  }
+};
+
+const fetchingAirportsReducer = (
+  state = { loading: 'idle', error: null },
+  action
+) => {
+  switch (action.type) {
+    case 'airports/pending':
+      return { ...state, loading: 'pending' };
+    case 'airports/fulfilled':
+      return { ...state, loading: 'succeeded' };
+    case 'airports/error':
+      return { error: action.error, loading: 'rejected' };
+    default:
+      return state;
+  }
+};
+
+export const reducer = combineReducers({
+  dates: availableDatesReducer,
+  airports: combineReducers({
+    reducer: airportsReducer,
+    fetchStatus: fetchingAirportsReducer,
+  }),
+});

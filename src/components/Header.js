143158b7c9999ad60ec5f0aa1d3784094e3c5608
Isaak Haas <isaakhaas96@gmail.com>
2022-02-16 19:06:29 -0600

    feat: added material ui icons and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import '../styles/header.css';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
 
 const Header = () => {
   return (
     <div className='header-container'>
       <div className='logo'>
-        <img src='' alt='logo' />
+        <FlightTakeoffIcon fontSize="large"/>
+        <p>TrueFligth</p>
       </div>
       <div className='shopping-cart'>
-        <img src='' alt='shopping' />
+        <ShoppingBagIcon fontSize="large" />
       </div>
     </div>
   );

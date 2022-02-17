9c0663964401b45a53b6fed22b0b4108551238b0
Isaak Haas <isaakhaas96@gmail.com>
2022-02-16 18:29:51 -0600

    feat: creation of header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 import Dashboard from './components/Dashboard';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Dashboard />
+    <div>
+      <Header />
+      <Dashboard />
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import '../styles/header.css';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <div className='logo'>
+        <img src='' alt='logo' />
+      </div>
+      <div className='shopping-cart'>
+        <img src='' alt='shopping' />
+      </div>
+    </div>
+  );
+};
+
+export default Header;

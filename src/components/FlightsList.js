66c42b2e9ddf8ff98ccdece281289ef119a1ab77
Isaak Haas <isaakhaas96@gmail.com>
2022-02-19 00:47:55 -0600

    feat: creation of flight list component

---
+++ src/components/FlightsList.js
@@ -0,0 +1,46 @@
+import moment from 'moment';
+
+const FlightsList = ({ handleSelectedFlight, flights }) => {
+  moment.locale('en');
+  debugger;
+  return (
+    <>
+      {flights.journeys.map((x) => {
+        return (
+          <div
+            key={x.key}
+            className='flights-list'
+            style={x.isSoldout ? { pointerEvents: 'none', opacity: '0.4' } : {}}
+          >
+            <li>
+              <div className='flights-departure-container'>
+                <p>{moment(x.departureDate).format('H:m')} hrs</p>
+                <p>{x.origin.code}</p>
+              </div>
+              <div className='flights-departure-container'>
+                <p> {moment(x.arrivalDate).format('H:m')} hrs</p>
+                <p> {x.destination.code}</p>
+              </div>
+              <div className='flights-departure-container'>
+                <p>Duration </p>
+                <p>{x.journeyDuration} hrs</p>
+              </div>
+              <div
+                id={x.key}
+                className='flights-departure-price'
+                onClick={handleSelectedFlight}
+              >
+                <div id={x.key} className='flights-departure-container '>
+                  <p id={x.key}>Price</p>
+                  <p id={x.key}>${x.fare.amount}</p>
+                </div>
+              </div>
+            </li>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default FlightsList;
--- src/components/SelectFlight.js
+++ src/components/SelectFlight.js
@@ -1,13 +1,12 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import moment from 'moment';
 
+import FlightsList from './FlightsList';
 import Wrapper from './Wrapper';
 import Loading from './Loading';
 
 import { fetchFlights } from '../features/reducer';
-import { getFlights } from '../features/flightReducer';
 import { availableAirports } from '../features/airportReducer';
 import { selectDates } from '../features/airportReducer';
 
@@ -16,18 +15,20 @@ import '../styles/selectFlights.css';
 const SelectFlight = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const payload  = useSelector(getFlights);
+
   const { input, passagers, route } = useSelector(availableAirports);
   const dates = useSelector(selectDates);
+  const flights = useSelector(
+    ({ flights: { flightReducer } }) => flightReducer
+  );
   const { loading } = useSelector(
     ({ flights: { fetchingFlightsReducer } }) => fetchingFlightsReducer
   );
-
+  debugger;
   const handleSelectedFlight = ({ target }) => {
+    debugger;
     const { id } = target;
-    const [selectedFlight] = payload.journeys.filter(
-      (x) => x.key === id
-    );
+    const [selectedFlight] = flights.journeys.filter((x) => x.key === id);
     const amount = selectedFlight.fare.amount;
     const newState = {
       id: id,
@@ -44,75 +45,22 @@ const SelectFlight = () => {
     });
 
     dispatch({
-      type: 'airports/reset',
+      type: 'airports/resetselected',
     });
     navigate('/trueflight');
   };
 
-  //TODO create a component
-
-  const getFlightsByDate = () => {
-    debugger;
-    if (!payload) {
-      return null;
-    }
-
-    if (loading === 'rejected') {
-      return <h1>Something went wrong</h1>;
-    }
-    moment.locale('en');
-
-    debugger;
-    const flights = payload;
-    if (Object.keys(flights).length === 0) {
-      dispatch({ type: 'flights/error' });
-      return;
-    }
-
-    return flights.journeys.map((x) => {
-      return (
-        <div
-          key={x.key}
-          className='flights-list'
-          style={x.isSoldout ? { pointerEvents: 'none', opacity: '0.4' } : {}}
-        >
-          <li>
-            <div className='flights-departure-container'>
-              <p>{moment(x.departureDate).format('H:m')} hrs</p>
-              <p>{x.origin.code}</p>
-            </div>
-            <div className='flights-departure-container'>
-              <p> {moment(x.arrivalDate).format('H:m')} hrs</p>
-              <p> {x.destination.code}</p>
-            </div>
-            <div className='flights-departure-container'>
-              <p>Duration </p>
-              <p>{x.journeyDuration} hrs</p>
-            </div>
-            <div
-              id={x.key}
-              className='flights-departure-price'
-              onClick={handleSelectedFlight}
-            >
-              <div id={x.key} className='flights-departure-container '>
-                <p id={x.key}>Price</p>
-                <p id={x.key}>${x.fare.amount}</p>
-              </div>
-            </div>
-          </li>
-        </div>
-      );
-    });
-  };
-
   useEffect(() => {
-    debugger;
     dispatch(fetchFlights(route));
   }, [dispatch, route]);
 
-  if (loading === 'rejected') {
-    return <h1>Something went wrong :(</h1>;
+  debugger;
+  if (Object.keys(flights).length === 0 || loading === 'pending') {
+    return <Loading />;
+  } else if (loading === 'rejected') {
+    return <h1>Something went wrong in our side 🥺</h1>;
   }
+
   return (
     <>
       {loading !== 'pending' ? (
@@ -126,7 +74,14 @@ const SelectFlight = () => {
               <p>Number of passagers: {passagers.number}</p>
             </div>
           </div>
-          <div className='flights-data-container'>{getFlightsByDate()}</div>
+          <div className='flights-data-container'>
+            {
+              <FlightsList
+                flights={flights}
+                handleSelectedFlight={handleSelectedFlight}
+              />
+            }
+          </div>
         </Wrapper>
       ) : (
         <Loading />

a07f38622d6a3513287c8cc32e08c323a7553bf3
Isaak Haas <isaakhaas96@gmail.com>
2022-02-21 13:39:28 -0600

    feat: added env variables to firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBRzmS1Mqy_mpArS6NUxgg4Iv0Tene74-Y',
-  authDomain: 'trueflight-90e12.firebaseapp.com',
-  projectId: 'trueflight-90e12',
-  storageBucket: 'trueflight-90e12.appspot.com',
-  messagingSenderId: '828539233689',
-  appId: '1:828539233689:web:1ea5a623bcfc2acfa3df08',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

ecb9c15ce312be6472461e90a08b52262be3d73b
Isaak Haas <isaakhaas96@gmail.com>
2022-02-21 20:50:28 -0600

    feat: added new coupons

--- src/reducers/types.js
+++ src/reducers/types.js
@@ -15,17 +15,16 @@ export const RESET_BOOKING = 'booking/reset';
 export const SET_FLIGHTS = 'flights/fulfilled';
 export const RESET_FLIGHTS = 'flights/reset';
 
-
-
-export const LOADING_UI = 'ui/pending'
-export const STOP_LOADING_UI = 'ui/fulfilled'
-export const ERROR_UI = 'ui/error'
-
+export const LOADING_UI = 'ui/pending';
+export const STOP_LOADING_UI = 'ui/fulfilled';
+export const ERROR_UI = 'ui/error';
 
 export const coupons = {
   'HIRE ME': 100,
   'HIRE Me :D': 100,
   'HIRE ME :)': 100,
   'I WANT TO TRAVEL': 50,
-  'LET ME OUT': 25
+  'LET ME OUT': 25,
+  UWU: 10,
+  uwu: 10,
 };
